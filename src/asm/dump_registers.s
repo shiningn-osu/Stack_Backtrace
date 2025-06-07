0a385d6cc1224e4940f9c8ad9a3edb48a36e0668
shiningn-osu <shiningn@oregonstate.edu>
2025-06-07 10:47:50 -0700

    Update dump_registers.s
    Added Functionality to register dump

--- src/asm/dump_registers.s
+++ src/asm/dump_registers.s
@@ -3,8 +3,27 @@
 .type dump_registers, @function
 dump_registers:
 subq $(16*8), %rsp
+
+push %r15
+push %r14
+push %r13
+push %r12
+push %r11
+push %r10
+push %r9
+push %r8
+push %rsp
+push %rbp
+push %rdi
+push %rsi
+push %rdx
+push %rcx
+push %rbx
+push %rax
+
 movq %rsp, %rdi
 call _debug_dump_registers
 addq $(16*8), %rsp
+leave
 ret
 
